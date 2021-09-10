710c0a40809eede589283db279adbf73cf86aa93
Margarita <margaritalomova666@gmail.com>
2021-09-10 18:25:52 +0300

    First

---
+++ Assets/MenuPackage/Scripts/LevelButtonController.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelButtonController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text levelNumber;
+    [SerializeField]
+    private Image image;
+
+    public void Set(int buildIndex, Color textColor, TMP_FontAsset font = null, Sprite sprite = null)
+    {
+        levelNumber.text = $"{buildIndex}";
+        levelNumber.color = textColor;
+        if (font != null)
+        {
+            levelNumber.font = font;
+        }
+
+        if (sprite != null)
+        {
+            image.sprite = sprite;
+            image.type = Image.Type.Simple;
+            image.preserveAspect = true;
+        }
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadSceneAsync(Convert.ToInt32(levelNumber.text));
+    }
+}
