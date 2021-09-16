3cfa18c2928e30b8f00851d8ea0b5fd549277c02
Margarita <margaritalomova666@gmail.com>
2021-09-16 13:21:34 +0300

    Editor scripts edit

--- Editor/Scripts/EditorController.cs
+++ Editor/Scripts/EditorController.cs
@@ -1,13 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
+[InitializeOnLoad]
 public class EditorController : EditorWindow
 {
-    [MenuItem("Example/Display simple Window")]
-    static void Initialize()
+    private bool packageDownloaded;
+    private static EditorController window;
+
+    static EditorController()
     {
-        //EditorWindowTest window = (EditorWindowTest)EditorWindow.GetWindow(typeof(EditorWindowTest), true, "My Empty Window");
+        window = (EditorController)GetWindow(typeof(EditorController));
+    }
+
+    private void OnGUI()
+    {
+        packageDownloaded = Directory.Exists("Assets/Hyper Casyal Package");
+        EditorController window = (EditorController)GetWindow(typeof(EditorController));
+
+        if (!packageDownloaded)
+        {
+            window.Show();
+            GUILayout.BeginVertical(EditorStyles.helpBox);
+            {
+                GUILayout.Label("To start working with HC_Package files, you need to import them into the project.", EditorStyles.boldLabel);
+                GUILayout.Space(5f);
+
+                if (GUILayout.Button("Import"))
+                {
+                    AssetDatabase.ImportPackage("Packages/HC_Package/Editor/HC_Package.unitypackage", false);  //In real package
+                    //AssetDatabase.ImportPackage("Assets/HC_Package.unitypackage", false);   //To test
+
+                    window.Close();
+                }
+                GUILayout.Space(5f);
+            }
+            GUILayout.EndVertical();
+        }
+        else
+        {
+            window.Close();
+        }
     }
 }
